22c15896db44761cfffa8b10847a7f9469b0d536
Bruno <brunosalernorocha@gmail.com>
2020-01-21 19:41:48 -0300

    Controller tests done

--- spec/controllers/aired_date_controller_spec.rb
+++ spec/controllers/aired_date_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe AiredDateController, type: :controller do
+RSpec.describe AiredDatesController, type: :controller do
 
+	context "GET #show/1" do
+		it "returns a successful response" do
+			get :show, params: {id: 1}
+			expect(response).to have_http_status(:success)
+		end
+	end
+
+	context "GET #show/1" do
+		it "returns a specific json" do
+			get :show, params: {id: 1}
+			parsed_response = JSON.parse(response.body)
+			expect(parsed_response.keys).to contain_exactly('aired_date')
+		end
+	end
 end

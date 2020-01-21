8e84471c8d4e1012e4541c74b008782f55f5713b
Bruno <brunosalernorocha@gmail.com>
2020-01-20 23:06:31 -0300

    Fixing controller json rendering

--- app/controllers/aired_dates_controller.rb
+++ app/controllers/aired_dates_controller.rb
@@ -3,9 +3,9 @@ class AiredDatesController < ApplicationController
 		srvc = RequestService.new(params[:id])
 		date = srvc.getAiredDate		   
 		if date.nil?
-			render json: { status: 503 }
+			render json: {}, status: 503
 		else
-			render json: { status: 200, aired_date: date }
+			render json: { aired_date: date }
 		end
 	end
 end

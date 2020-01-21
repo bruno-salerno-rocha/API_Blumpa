6388c5282031d2b638e0fcffd46bf462893397b8
Bruno <brunosalernorocha@gmail.com>
2020-01-21 00:02:37 +0000

    Making request method

--- app/services/request_service.rb
+++ app/services/request_service.rb
@@ -1,8 +1,17 @@
+require 'httparty'
+
 class RequestService
+
 	def initialize(id)
 		@id = id
 	end
 
 	def getAiredDate
+		response = HTTParty.get("https://rickandmortyapi.com/api/character/#{@id}")
+		ch_data = JSON.parse response.body
+		ep_id = ch_data['episode'].first.split('/').last
+		response = HTTParty.get("https://rickandmortyapi.com/api/episode/#{ep_id}")
+		ep_data = JSON.parse response.body
+		ep_data['air_date']
 	end
 end

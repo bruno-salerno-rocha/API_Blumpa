fb5ab97409ba3f73b51c51b175d9477f8a9278e3
Bruno <brunosalernorocha@gmail.com>
2020-01-21 21:14:39 -0300

    Controller code improved

--- app/controllers/aired_dates_controller.rb
+++ app/controllers/aired_dates_controller.rb
@@ -1,11 +1,16 @@
 class AiredDatesController < ApplicationController
 	def show
-		srvc = RequestService.new(params[:id])
-		date = srvc.getAiredDate		   
 		if date.nil?
 			render json: {}, status: 503
 		else
 			render json: { aired_date: date }
 		end
+	end
+
+	private
+
+	def date
+		srvc = RequestService.new(params[:id])
+		date = srvc.getAiredDate
 	end
 end
